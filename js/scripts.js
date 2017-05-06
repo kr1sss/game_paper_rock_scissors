bab0ecdd959ec57e53c7d0a82fab52b6bddf4daf
Krzysiek <kr1sss@wp.pl>
2017-05-06 21:29:24 +0200

    gierka

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function() {
+	list.innerHTML += '<li>item' + ' ' + document.getElementsByTagName('li').length;
+})
